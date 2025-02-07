8fa9542af9a813bb48154d1e9bb2b287baa52002
Jon Sultana <jonsultana@outlook.com>
2025-02-06 22:15:31 -0500

    Feature/appbar 3 (#5)
    * Top app bar with minimize, maximize, and close buttons.
    
    * Updating styling.
    
    * Fixed styling build errors.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/theme-provider';
 import { Header } from '@/components/ui/header';
+import AppBar from '@/components/ui/app-bar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -35,6 +36,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <AppBar />
           <Header />
           {children}
         </ThemeProvider>
---
+++ components/ui/app-bar/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+export default function AppBar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.buttonGroup}>
+        <span className={styles.minimize} />
+        <span className={styles.maximize} />
+        <span className={styles.close} />
+      </div>
+    </div>
+  );
+}
