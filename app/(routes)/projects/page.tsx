f45e8e7ed78fddfc45f1003c40ec0e373de18878
Jon Sultana <jonsultana@outlook.com>
2025-04-21 17:55:08 -0400

    Updated projects to view to state that it is under construction.

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,9 +1,21 @@
+import { Construction } from 'lucide-react';
 import * as React from 'react';
 
 const ProjectsPage = () => {
   return (
-    <div className="p-[10px]">
-      <h3>Projects view.</h3>
+    <div className="flex flex-col gap-[10px] m-auto w-[400px]">
+      <div className="flex flex-col items-center justify-center">
+        <Construction size={100} />
+        <h3 className="text-lg font-extrabold">
+          This page is currently under construction
+        </h3>
+      </div>
+      <div>
+        <p>
+          Check back soon! In the meantime, feel free to explore the rest of the
+          site.
+        </p>
+      </div>
     </div>
   );
 };
