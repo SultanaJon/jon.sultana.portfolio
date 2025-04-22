530a4d8a17c54fe3b2c72a931fdc1314eecc4fca
Jon Sultana <jonsultana@outlook.com>
2025-04-21 22:05:51 -0400

    Updated the about page to be more responsive. (#30)

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -3,6 +3,7 @@
 import CodeBlock from '@/app/(components)/ui/code-block';
 import * as React from 'react';
 import CodeEditor from '@/app/(components)/ui/code-editor';
+import cx from 'classnames';
 
 const AboutPage = () => {
   const code = `import { hire } from 'hire';
@@ -19,8 +20,13 @@ const developer = new hire({
 developer.startBuilding();`;
 
   return (
-    <div className="flex gap-[5rem] w-full p-[8rem]">
-      <section className="max-w-4xl flex flex-col gap-[1.5rem]">
+    <div className={cx('flex overflow-auto flex-col', 'lg:flex-row')}>
+      <section
+        className={cx(
+          'w-full flex flex-col gap-[1.5rem] py-[2rem] px-[5rem]',
+          'lg:w-[50%] lg:py-[5rem] lg:ps-[5rem] lg:pe-[2.5rem]'
+        )}
+      >
         <div>
           <h1 className="text-6xl font-bold mb-2 text-[--primary]">
             Hi, I&apos;m Jon Sultana
@@ -33,18 +39,23 @@ developer.startBuilding();`;
           <p className="text-xl font-light">
             I am a Senior Software Engineer who enjoys building both frontend
             and backend applications that are fast, intuitive, and thoughtfully
-            designed. Whether it’s crafting user interfaces or architecting
+            designed. Whether it&apos;s crafting user interfaces or architecting
             scalable APIs, I take pride in solving problems through clean,
-            maintainable code. When I’m not deep in a code editor, you’ll likely
-            find me spending time with my family, exploring the natural beauty
-            of Northern Michigan, or tearing through the trails on my mountain
-            bike. Thanks for stopping by. Before you go, be sure to check out my
-            REST client for more information about me and any recent projects
-            I’ve been working on!
+            maintainable code. When I&apos;m not deep in a code editor,
+            you&apos;ll likely find me spending time with my family, exploring
+            the natural beauty of Northern Michigan, or tearing through the
+            trails on my mountain bike. Thanks for stopping by. Before you go,
+            be sure to check out my REST client for more information about me
+            and any recent projects I&apos;ve been working on!
           </p>
         </div>
       </section>
-      <section className="w-full">
+      <section
+        className={cx(
+          'w-full py-[2rem] px-[5rem]',
+          'lg:w-[50%] lg:py-[5rem] lg:ps-[2.5rem] lg:pe-[5rem]'
+        )}
+      >
         <div className="flex flex-col gap-[2rem]">
           <CodeEditor tabs={['>_terminal']}>
             <CodeBlock code={`npm install hire`} lang="shell" />
