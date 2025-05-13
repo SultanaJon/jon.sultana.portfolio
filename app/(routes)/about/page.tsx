0b0260c12cd20902ba7be53a2b2d6cb9895c97ce
Jon Sultana <jonsultana@outlook.com>
2025-05-13 19:34:28 -0400

    Moved about page content to user config for easier access. (#42)

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -4,21 +4,16 @@ import CodeBlock from '@/app/(components)/ui/code-block';
 import * as React from 'react';
 import CodeEditor from '@/app/(components)/ui/code-editor';
 import cx from 'classnames';
-import { userJobTitle, userName } from '@/config/user.config';
+import {
+  userCodeSnippet,
+  userJobTitle,
+  userLocation,
+  userName,
+  userStory,
+} from '@/config/user.config';
 
 const AboutPage = () => {
-  const code = `import { hire } from 'hire';
-
-// Initialize the package with your developer details
-const developer = new hire({
-    name: ${userName},
-    role: ${userJobTitle},
-    skills: ['JavaScript', 'HTML', 'CSS', 'Node.js', 'React'],
-    passion: 'Building web applications that improve the user experience.',
-  });
-
-// Start working with your new developer!
-developer.startBuilding();`;
+  const code = userCodeSnippet;
 
   return (
     <div className={cx('flex flex-col', 'lg:flex-row')}>
@@ -33,20 +28,11 @@ developer.startBuilding();`;
             {`Hi, I'm ${userName}`}
           </h1>
           <h3 className="text-2xl font-medium">
-            {`A ${userJobTitle} based in Dummy City`}
+            {`A ${userJobTitle} based in ${userLocation}`}
           </h3>
         </div>
         <div className="flex flex-col gap-[1rem]">
-          <p className="text-xl font-light">
-            {`I am a ${userJobTitle} who enjoys building both frontend
-            and backend applications that are fast, intuitive, and thoughtfully
-            designed. Whether it's crafting user interfaces or learning
-            about new technologies, I take pride in solving problems through clean,
-            maintainable code. When I'm not deep in a code editor,
-            you'll likely find me exploring new hobbies or enjoying outdoor activities.
-            Thanks for stopping by. Before you go,
-            be sure to check out my work and recent projects!`}
-          </p>
+          <p className="text-xl font-light">{userStory}</p>
         </div>
       </section>
       <section
