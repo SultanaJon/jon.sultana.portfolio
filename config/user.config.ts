9606d0e55dd9c34c08d5b62370a11650494de752
Jon Sultana <jonsultana@outlook.com>
2025-05-13 20:26:51 -0400

    Update code snippet in user config. (#45)

--- config/user.config.ts
+++ config/user.config.ts
@@ -14,23 +14,31 @@ export const userSocialLinks: SocialLinks = {
   linkedin: 'https://www.linkedin.com/in/johndoe',
 };
 
-const userSkills = ['JavaScript', 'HTML', 'CSS', 'Node.js', 'React'];
-
 const userPassion =
   'Building web applications that improve the user experience.';
+
+const userObject = {
+  name: userName,
+  role: userJobTitle,
+  skills: [
+    'JavaScript',
+    'TypeScript',
+    'HTML',
+    'CSS',
+    'Next.js',
+    'React',
+    '...',
+  ],
+  passion: userPassion,
+};
 
 export const userCodeSnippet = `import { hire } from 'hire';
 
 // Initialize the package with your developer details
-const developer = new hire({
-    name: "${userName}",
-    role: "${userJobTitle}",
-    skills: ${JSON.stringify(userSkills)},
-    passion: "${userPassion}",
-  });
+const developer = new hire();
 
 // Start working with your new developer!
-developer.startBuilding();`;
+developer.startBuilding(${JSON.stringify(userObject, null, 2)});`;
 
 export const userStory = `I am a ${userJobTitle} who enjoys building both frontend
             and backend applications that are fast, intuitive, and thoughtfully
