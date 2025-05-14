b32e96238546d1afab920e7c1fced11453f4fa87
Jon Sultana <jonsultana@outlook.com>
2025-05-13 20:16:08 -0400

    user config updates.

--- config/user.config.ts
+++ config/user.config.ts
@@ -1,5 +1,5 @@
-export const userName: string = 'John Doe';
-export const userJobTitle: string = 'Software Engineer';
+export const userName: string = 'Jonathan Sultana';
+export const userJobTitle: string = 'Senior Software Engineer';
 export const userLocation: string = 'Michigan';
 
 interface SocialLinks {
@@ -9,34 +9,45 @@ interface SocialLinks {
 }
 
 export const userSocialLinks: SocialLinks = {
-  email: 'john.doe@example.com',
-  github: 'https://github.com/johndoe',
-  linkedin: 'https://www.linkedin.com/in/johndoe',
+  email: 'jon.michael.sultana@gmail.com',
+  github: 'https://github.com/SultanaJon',
+  linkedin: 'https://www.linkedin.com/in/jonathan-sultana-29b56558/',
 };
-
-const userSkills = ['JavaScript', 'HTML', 'CSS', 'Node.js', 'React'];
 
 const userPassion =
   'Building web applications that improve the user experience.';
 
+const userObject = {
+  name: userName,
+  role: userJobTitle,
+  skills: [
+    'JavaScript',
+    'TypeScript',
+    'HTML',
+    'CSS',
+    'Next.js',
+    'React',
+    '...',
+  ],
+  passion: userPassion,
+};
+
 export const userCodeSnippet = `import { hire } from 'hire';
 
 // Initialize the package with your developer details
-const developer = new hire({
-    name: "${userName}",
-    role: "${userJobTitle}",
-    skills: ${JSON.stringify(userSkills)},
-    passion: "${userPassion}",
-  });
+const developer = new hire();
 
 // Start working with your new developer!
-developer.startBuilding();`;
+developer.startBuilding(${JSON.stringify(userObject, null, 2)});`;
 
-export const userStory = `I am a ${userJobTitle} who enjoys building both frontend
-            and backend applications that are fast, intuitive, and thoughtfully
-            designed. Whether it's crafting user interfaces or learning
-            about new technologies, I take pride in solving problems through clean,
-            maintainable code. When I'm not deep in a code editor,
-            you'll likely find me exploring new hobbies or enjoying outdoor activities.
-            Thanks for stopping by. Before you go,
-            be sure to check out my work and recent projects!`;
+export const userStory = `
+  I am a ${userJobTitle} who enjoys building both frontend 
+  and backend applications that are fast, intuitive, and
+  thoughtfully designed. Whether it's crafting user interfaces
+  or architecting scalable APIs, I take pride in solving problems
+  through clean, maintainable code. When I'm not deep in a code
+  editor, you'll likely find me spending time with my family,
+  exploring the natural beauty of Northern Michigan, or tearing
+  through the trails on my mountain bike. Thanks for stopping by.
+  Before you go, be sure to check out my REST client for more
+  information about me and any recent projects I've been working on!`;
