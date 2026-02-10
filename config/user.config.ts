4e68017dd814cc0f8c046999bda482f88f246315
= <jonsultana@outlook.com>
2026-02-10 00:52:15 -0500

    readme updates

--- config/user.config.ts
+++ config/user.config.ts
@@ -18,7 +18,7 @@ export const userCodeSnippet = `
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello", () => "Hello World!");
 
 app.Run();
 `;
