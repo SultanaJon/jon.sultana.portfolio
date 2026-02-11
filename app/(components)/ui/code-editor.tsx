f7f8d04f638c8577bea759beff99293b5e02d606
= <jonsultana@outlook.com>
2026-02-10 21:11:06 -0500

    Make the code editor borders more visible. The editor was blending in with the page.

--- app/(components)/ui/code-editor.tsx
+++ app/(components)/ui/code-editor.tsx
@@ -1,19 +1,19 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import GripBar from './grip-bar';
 import CodeFrame from './code-frame';
 import { userCodeSnippet } from '@/config/user.config';
 import Panel from './panel';
+import { useTheme } from 'next-themes';
 
 const CodeEditor = () => {
+  const { theme } = useTheme();
   const code = userCodeSnippet;
 
   return (
-    <div className="rounded overflow-hidden bg-(--background-secondary-contrast)">
+    <div className={`rounded overflow-hidden bg-(--background-secondary-contrast) ${theme == 'light' ? 'border solid border-gray-300': ''}`}>
       <GripBar tabs={['Program.cs']} />
       <div className='h-[300px] max-h-[300px]'>
         <CodeFrame code={code} lang="csharp" />
       </div>
-      <hr className='bg-(--background-contrast)' />
       <Panel />
     </div >
   );
